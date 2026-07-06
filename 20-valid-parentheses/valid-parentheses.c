e274e1a73f644dd095480c1d1bdf143ac1db8a6d
djtank2307-hub <djtank2307@gmail.com>
2026-07-06 22:04:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (30.86%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.c
@@ -0,0 +1,22 @@
+bool isValid(char* s) {
+
+     char stack[10000];   
+    int top = -1;       
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        char c = s[i];
+        if (c == '(' || c == '{' || c == '[') {
+            stack[++top] = c;   // push
+        } else {
+            if (top == -1) return false; 
+            char topChar = stack[top--]; 
+            if ((c == ')' && topChar != '(') ||
+                (c == '}' && topChar != '{') ||
+                (c == ']' && topChar != '[')) {
+                return false;
+            }
+        }
+    }
+    return top == -1;
+
+}
